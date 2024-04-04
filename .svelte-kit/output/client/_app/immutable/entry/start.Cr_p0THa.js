c7d23e9e25abc82322bd3d8b124aafb175feaae5
Javid N <cavidnebi@gmail.com>
2024-04-05 00:34:52 +0400

    yo10

---
+++ .svelte-kit/output/client/_app/immutable/entry/start.Cr_p0THa.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BVVyiokN.js";export{t as start};
--- src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<div
-	style="display: grid;
-place-content: center;
-height: calc(100dvh - 80px);"
->
-	<h3>Xoş Gəldiniz!</h3>
-</div>

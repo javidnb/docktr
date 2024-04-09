c484fea6761df5105072180dd2ca7e4ba44d4f5d
Javid N <cavidnebi@gmail.com>
2024-04-09 14:00:26 +0400

    yo41

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -2,10 +2,13 @@
 	import { diseases } from '$lib/store/diseases';
 	import { session } from '$lib/session';
 	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
 
-	if (!$session.loggedIn) {
-		goto('./');
-	}
+	onMount(async () => {
+		if (!$session.loggedIn) {
+			goto('./login');
+		}
+	});
 
 	function handleSubmit(e: SubmitEvent) {
 		const formData = new FormData(e.target as HTMLFormElement);
@@ -20,63 +23,64 @@
 	}
 </script>
 
-<section>
-	<div class="jumbotron" style="padding-top: 2rem; background-color: #e2e9ef">
-		<h1 class="display-4">Admin Panel</h1>
-		<!-- <p class="lead">We connect you to doctors around the world!</p> -->
-		<hr />
-		<!-- <p>
+{#if $session.loggedIn}
+	<section>
+		<div class="jumbotron" style="padding-top: 2rem; background-color: #e2e9ef">
+			<h1 class="display-4">Admin Panel</h1>
+			<!-- <p class="lead">We connect you to doctors around the world!</p> -->
+			<hr />
+			<!-- <p>
 			It uses utility classes for typography and spacing to space content out within the larger
 			container.
 		</p> -->
-		<!-- <a class="btn btn-primary btn-lg" href="#" role="button">Doktorlar</a> -->
-	</div>
-</section>
-
-<div class="container-fluid">
-	<div class="row mt-3 justify-content-center">
-		<div class="col-md-5">
-			<h5>Həkim əlavə et</h5>
-			<hr />
+			<!-- <a class="btn btn-primary btn-lg" href="#" role="button">Doktorlar</a> -->
+		</div>
+	</section>
+	<div class="container-fluid">
+		<div class="row mt-3 justify-content-center">
+			<div class="col-md-5">
+				<h5>Həkim əlavə et</h5>
+				<hr />
+			</div>
+		</div>
+		<div class="row justify-content-center">
+			<div class="col-md-5">
+				<form on:submit|preventDefault={handleSubmit}>
+					<div class="form-group">
+						<label for="name">Adı Soyad</label>
+						<input
+							name="name"
+							type="text"
+							class="form-control"
+							id="name"
+							placeholder="Ad Soyad"
+							required
+						/>
+					</div>
+					<div class="form-group">
+						<label for="profession">Uzmanlık</label>
+						<input
+							name="profession"
+							class="form-control"
+							list="professions"
+							id="profession"
+							placeholder="Uzmanlık"
+							required
+						/>
+						<datalist id="professions">
+							{#each diseases as item}
+								<option value={item.name} data-value={item.id}></option>
+							{/each}
+						</datalist>
+					</div>
+					<div class="form-group">
+						<label for="details">Məlumat</label>
+						<textarea name="details" class="form-control" id="details" rows="3"></textarea>
+					</div>
+					<button type="submit" class="btn btn-primary">Əlavə et</button>
+					<!-- formaction="?/register" -->
+				</form>
+			</div>
 		</div>
 	</div>
-	<div class="row justify-content-center">
-		<div class="col-md-5">
-			<form on:submit|preventDefault={handleSubmit}>
-				<div class="form-group">
-					<label for="name">Adı Soyad</label>
-					<input
-						name="name"
-						type="text"
-						class="form-control"
-						id="name"
-						placeholder="Ad Soyad"
-						required
-					/>
-				</div>
-				<div class="form-group">
-					<label for="profession">Uzmanlık</label>
-					<input
-						name="profession"
-						class="form-control"
-						list="professions"
-						id="profession"
-						placeholder="Uzmanlık"
-						required
-					/>
-					<datalist id="professions">
-						{#each diseases as item}
-							<option value={item.name} data-value={item.id}></option>
-						{/each}
-					</datalist>
-				</div>
-				<div class="form-group">
-					<label for="details">Məlumat</label>
-					<textarea name="details" class="form-control" id="details" rows="3"></textarea>
-				</div>
-				<button type="submit" class="btn btn-primary">Əlavə et</button>
-				<!-- formaction="?/register" -->
-			</form>
-		</div>
-	</div>
-</div>
+{/if}
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -8,14 +8,12 @@
 	import History from '$lib/components/profile/History.svelte';
 
 	export let data;
-
-	if (!$session.loggedIn) {
-		goto('./');
-	}
-
 	let userEmail: any = '';
 
 	onMount(async () => {
+		if (!$session.loggedIn) {
+			goto('./login');
+		}
 		const user: any = await data.getAuthUser();
 		userEmail = user.email;
 	});
@@ -32,11 +30,12 @@
 	}
 </script>
 
-<section>
-	<div class="jumbotron" style="padding-top: 2rem; background-color: #e2e9ef">
-		<button
-			class="btn mobileOnly"
-			style="position: absolute;
+{#if $session.loggedIn}
+	<section>
+		<div class="jumbotron" style="padding-top: 2rem; background-color: #e2e9ef">
+			<button
+				class="btn mobileOnly"
+				style="position: absolute;
 			left: 15px;
 			top: 9px;
 			background: #00000054;
@@ -45,136 +44,130 @@
 			align-items: center;
 			padding-inline: 14px 5px;
 			color: var(--primaryColor);"
-			class:d-none={!mobileComponent}
-			on:click={() => {
-				changeComponent(null, true);
-			}}><span class="material-symbols-outlined"> arrow_back_ios </span></button
-		>
-		<h1 class="display-4">Hesabım</h1>
-		<hr />
-	</div>
-</section>
+				class:d-none={!mobileComponent}
+				on:click={() => {
+					changeComponent(null, true);
+				}}><span class="material-symbols-outlined"> arrow_back_ios </span></button
+			>
+			<h1 class="display-4">Hesabım</h1>
+			<hr />
+		</div>
+	</section>
+	<section>
+		<div class="container mobileCont">
+			<div class="row mt-3 pb-5">
+				<div class="col-12 col-md-4 col-lg-3 col-xxl-2">
+					{#if !mobileComponent}
+						<ul class="list-group pcOnly w-100">
+							<button
+								class="list-group-item w-100"
+								on:click={() => {
+									changeComponent(ProfDetails);
+								}}
+								class:active={component == ProfDetails}
+							>
+								<span class="material-symbols-outlined"> for_you </span>Hesap Bilgileri
+							</button>
+							<button
+								class="list-group-item w-100"
+								on:click={() => {
+									changeComponent(History);
+								}}
+								class:active={component == History}
+							>
+								<span class="material-symbols-outlined"> history </span>Hastalık Geçmişi
+							</button>
+							<li class="list-group-item">
+								<span class="material-symbols-outlined"> draft </span>Dökümanlarım
+							</li>
+							<li class="list-group-item">
+								<span class="material-symbols-outlined"> description </span>Kayıtlarım
+							</li>
+							<li class="list-group-item">
+								<span class="material-symbols-outlined"> medication </span>İlaçlarım
+							</li>
+							<li class="list-group-item">
+								<span class="material-symbols-outlined"> event </span>İlaç hatırlatıcı
+							</li>
+							<li class="list-group-item">
+								<span class="material-symbols-outlined"> clinical_notes </span>Doktorlarım
+							</li>
+							<a class="list-group-item" href="./admin">
+								<span class="material-symbols-outlined"> admin_panel_settings </span>Admin
+							</a>
+							<a class="list-group-item" on:click={logout} href="../">
+								<span class="material-symbols-outlined"> logout </span>Çıkış
+							</a>
+						</ul>
+						<ul class="list-group mobileList mobileOnly w-100">
+							<button
+								class="list-group-item w-100"
+								on:click={() => {
+									changeComponent(ProfDetails, true);
+								}}
+							>
+								<span class="material-symbols-outlined"> for_you </span>Hesap Bilgileri
+							</button>
+							<button
+								class="list-group-item w-100"
+								on:click={() => {
+									changeComponent(History, true);
+								}}
+							>
+								<span class="material-symbols-outlined"> history </span>Hastalık Geçmişi
+							</button>
+							<li class="list-group-item">
+								<span class="material-symbols-outlined"> draft </span>Dökümanlarım
+							</li>
+							<li class="list-group-item">
+								<span class="material-symbols-outlined"> description </span>Kayıtlarım
+							</li>
+							<li class="list-group-item">
+								<span class="material-symbols-outlined"> medication </span>İlaçlarım
+							</li>
+							<li class="list-group-item">
+								<span class="material-symbols-outlined"> event </span>İlaç hatırlatıcı
+							</li>
+							<li class="list-group-item">
+								<span class="material-symbols-outlined"> clinical_notes </span>Doktorlarım
+							</li>
+							<a class="list-group-item" href="./admin">
+								<span class="material-symbols-outlined"> admin_panel_settings </span>Admin
+							</a>
+							<a class="list-group-item" on:click={logout} href="../">
+								<span class="material-symbols-outlined"> logout </span>Çıkış
+							</a>
+						</ul>
+					{/if}
+				</div>
 
-<section>
-	<div class="container mobileCont">
-		<div class="row mt-3 pb-5">
-			<div class="col-12 col-md-3 cl-lg-2">
-				{#if !mobileComponent}
-					<ul class="list-group pcOnly w-100">
-						<button
-							class="list-group-item w-100"
-							on:click={() => {
-								changeComponent(ProfDetails);
-							}}
-							class:active={component == ProfDetails}
-						>
-							<span class="material-symbols-outlined"> for_you </span>Hesap Bilgileri
-						</button>
-						<button
-							class="list-group-item w-100"
-							on:click={() => {
-								changeComponent(History);
-							}}
-							class:active={component == History}
-						>
-							<span class="material-symbols-outlined"> history </span>Hastalık Geçmişi
-						</button>
-						<li class="list-group-item">
-							<span class="material-symbols-outlined"> draft </span>Dökümanlarım
-						</li>
-						<li class="list-group-item">
-							<span class="material-symbols-outlined"> description </span>Kayıtlarım
-						</li>
-						<li class="list-group-item">
-							<span class="material-symbols-outlined"> medication </span>İlaçlarım
-						</li>
-						<li class="list-group-item">
-							<span class="material-symbols-outlined"> event </span>İlaç hatırlatıcı
-						</li>
-						<li class="list-group-item">
-							<span class="material-symbols-outlined"> clinical_notes </span>Doktorlarım
-						</li>
-						<a class="list-group-item" href="./admin">
-							<span class="material-symbols-outlined"> admin_panel_settings </span>Admin
-						</a>
-						<a class="list-group-item" on:click={logout} href="../">
-							<span class="material-symbols-outlined"> logout </span>Çıkış
-						</a>
-					</ul>
-					<ul
-						class="list-group mobileOnly w-100"
-						style="    
-						min-height: 75vh;
-						background: white;
-				}"
-					>
-						<button
-							class="list-group-item w-100"
-							on:click={() => {
-								changeComponent(ProfDetails, true);
-							}}
-						>
-							<span class="material-symbols-outlined"> for_you </span>Hesap Bilgileri
-						</button>
-						<button
-							class="list-group-item w-100"
-							on:click={() => {
-								changeComponent(History, true);
-							}}
-						>
-							<span class="material-symbols-outlined"> history </span>Hastalık Geçmişi
-						</button>
-						<li class="list-group-item">
-							<span class="material-symbols-outlined"> draft </span>Dökümanlarım
-						</li>
-						<li class="list-group-item">
-							<span class="material-symbols-outlined"> description </span>Kayıtlarım
-						</li>
-						<li class="list-group-item">
-							<span class="material-symbols-outlined"> medication </span>İlaçlarım
-						</li>
-						<li class="list-group-item">
-							<span class="material-symbols-outlined"> event </span>İlaç hatırlatıcı
-						</li>
-						<li class="list-group-item">
-							<span class="material-symbols-outlined"> clinical_notes </span>Doktorlarım
-						</li>
-						<a class="list-group-item" href="./admin">
-							<span class="material-symbols-outlined"> admin_panel_settings </span>Admin
-						</a>
-						<a class="list-group-item" on:click={logout} href="../">
-							<span class="material-symbols-outlined"> logout </span>Çıkış
-						</a>
-					</ul>
-				{/if}
-			</div>
-
-			{#if mobileComponent}
-				<div class="col-md-9 col-lg-9 kont px-0">
-					<div class="container">
-						<div class="row">
-							<div class="col-12 mobileOnly" style="min-height: 75dvh; background-color: white">
-								<div class="container">
-									<svelte:component this={mobileComponent} />
+				{#if mobileComponent}
+					<div class="col-md-9 col-lg-9 kont px-0">
+						<div class="container">
+							<div class="row">
+								<div class="col-12 mobileOnly" style="background-color: unset">
+									<div class="container">
+										<svelte:component this={mobileComponent} />
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			{:else}
-				<div class="col-md-9 col-lg-9 kont pcOnly">
-					<div class="container">
-						<div class="row">
-							<div class="col pcOnly">
-								<svelte:component this={component} />
+				{:else}
+					<div class="col-md-8 col-lg-9 col-xxl-10 kont pcOnly">
+						<div class="container">
+							<div class="row">
+								<div class="col pcOnly">
+									<svelte:component this={component} />
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			{/if}
+				{/if}
+			</div>
 		</div>
-	</div>
-</section>
+	</section>
+{/if}
 
 <style>
 	.kont {
@@ -204,6 +197,13 @@
 		font-weight: 450;
 		width: 100%;
 	}
+	.mobileList,
+	.mobileList .list-group-item {
+		background-color: unset;
+	}
+	.mobileList .list-group-item {
+		padding-inline: 0;
+	}
 	@media screen and (max-width: 768px) {
 		.list-group {
 			border-radius: 0px;
@@ -220,8 +220,17 @@
 			color: black;
 			font-weight: 400;
 		}
-		.mobileCont {
-			padding-inline: 0;
+		.kont {
+			background-color: unset;
+			box-shadow: unset;
+		}
+	}
+	@media screen and (min-width: 768px) {
+		.pcOnly {
+			display: inline-block;
+		}
+		.mobileOnly {
+			display: none;
 		}
 	}
 </style>

1a87ebbaf64a0ec29f6d82ad6237339458437438
Javid N <cavidnebi@gmail.com>
2024-04-18 17:12:39 +0400

    v$(node -p -e require('../package.json').version)

--- src/lib/components/Appointment.svelte
+++ src/lib/components/Appointment.svelte
@@ -1,17 +1,47 @@
 <script lang="ts">
-	let date = new Date();
+	import { onMount } from 'svelte';
+
+	const currentDate = new Date();
+	currentDate.setDate(currentDate.getDate() + 1);
+	const year = currentDate.getFullYear();
+	const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+	const day = String(currentDate.getDate()).padStart(2, '0');
+	const tomorrow = `${year}-${month}-${day}`;
+
+	onMount(async () => {
+		console.log(tomorrow);
+	});
+
+	function handleSubmit(e: SubmitEvent) {
+		const formData = new FormData(e.target as HTMLFormElement);
+		const data: any = {};
+		for (let field of formData) {
+			const [key, value] = field;
+			data[key] = value;
+		}
+		console.log(data);
+		// let profession = diseases.find((d) => d.name == data.profession);
+		// console.log(profession?.id);
+	}
 </script>
 
 <section>
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col">
-				<form class="d-flex flex-column row-gap-3">
+				<form class="d-flex flex-column row-gap-3" on:submit|preventDefault={handleSubmit}>
 					<label for="date">Tarix</label>
-					<input class="form-control" type="date" id="date" value={date} />
+					<input
+						name="date"
+						class="form-control"
+						type="date"
+						id="date"
+						value={tomorrow}
+						min={tomorrow}
+					/>
 					<label for="time">Saat</label>
-					<input class="form-control" type="time" id="time" />
-					<button class="btn btn-primary">Göndər</button>
+					<input name="time" class="form-control" type="time" id="time" min="09:00" max="21:00" />
+					<button type="submit" class="btn btn-primary">Randevu al</button>
 				</form>
 			</div>
 		</div>
--- src/routes/doctors/[slug]/+page.svelte
+++ src/routes/doctors/[slug]/+page.svelte
@@ -6,14 +6,36 @@
 	import { session } from '$lib/session';
 	import Appointment from '$lib/components/Appointment.svelte';
 	import Modal from '$lib/helpers/Modal.svelte';
+	import { dataLoading } from '$lib/store/dataStore';
 
 	$: doctor = $doctors.find((d) => d.slug == $page.params.slug);
 	// $: console.log('brenc: ', doctor?.branches);
+	let btnCommentText: string = 'Göndər';
+	let btnCommentDisabled: boolean = false;
 	let truncate: boolean = true;
-	let selectedStarPoint: number;
+	let selectedStarPoint: number = 0;
 	let showModal: boolean = false;
+	let comments: any = [];
+	let alreadyCommented: boolean = false; // to allow commenting only once
 
-	onMount(async () => {});
+	onMount(async () => {
+		getComments();
+	});
+
+	async function getComments() {
+		try {
+			let time = new Date().getTime();
+			const response = await fetch(
+				`https://tekoplast.az/docktr/api/?comments&doctor=${doctor?.id}&t=${time}`
+			);
+			const result = await response.json();
+			comments = result;
+			if (comments.find((c: any) => c.userId == $session.user?.uid)) alreadyCommented = true;
+			console.log('comments: ', result);
+		} catch (error) {
+			console.error(error);
+		}
+	}
 
 	function getBranchName(id: number) {
 		let d = diseases.find((d) => d.id == id);
@@ -27,6 +49,44 @@
 
 	function truncateString(str: string, maxLength: number) {
 		return str.length > maxLength ? str.slice(0, maxLength) + '...' : str;
+	}
+
+	async function postComment(e: SubmitEvent) {
+		dataLoading.set(true);
+		btnCommentText = 'Gözləyin';
+		btnCommentDisabled = true;
+		const formData = new FormData(e.target as HTMLFormElement);
+		const data: any = {};
+		for (let field of formData) {
+			const [key, value] = field;
+			data[key] = value;
+		}
+		let postData = {
+			userId: $session.user?.uid,
+			doctorId: doctor?.id,
+			comment: data.comment,
+			star: selectedStarPoint
+		};
+
+		let dataToPost = { table: 'comments', data: { ...postData } };
+		const response = await fetch('https://tekoplast.az/docktr/api/?postData', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ ...dataToPost }),
+			cache: 'no-cache'
+		});
+
+		if (response.ok) {
+			console.log('Success');
+			btnCommentText = 'Uğurlu! Şərhiniz təsdiq gözləyir..';
+			dataLoading.set(false);
+		} else {
+			console.log('error');
+			btnCommentText = 'Xəta :(';
+			dataLoading.set(false);
+		}
 	}
 </script>
 
@@ -67,8 +127,15 @@
 						/>
 					</div>
 					<div class="col-9 col-md-10 d-flex flex-column align-items-center">
+						<button
+							class="btn btn-outline-primary d-flex justify-content-center align-items-center btnRandevu"
+							on:click={() => (showModal = true)}
+							><span class="material-symbols-outlined">local_library</span><span
+								style="margin-inline: auto;">Randevu Al</span
+							></button
+						>
 						{#if doctor?.branches}
-							<div class="branch d-flex flex-wrap column-gap-3">
+							<div class="branch d-flex flex-wrap column-gap-3 justify-content-center">
 								{#each doctor.branches as br}
 									<a style="color: var(--primaryColor)" href="../branches/{getBranchSlug(br)}"
 										>{getBranchName(br)}</a
@@ -76,13 +143,6 @@
 								{/each}
 							</div>
 						{/if}
-						<button
-							class="btn btn-outline-primary d-flex justify-content-center align-items-center py-3 btnRandevu"
-							on:click={() => (showModal = true)}
-							><span class="material-symbols-outlined">local_library</span><span
-								style="margin-inline: auto;">Randevu Al</span
-							></button
-						>
 					</div>
 				</div>
 				<div class="row mt-3">
@@ -109,10 +169,15 @@
 				</div>
 				<div class="row mt-3">
 					<div class="card p-3">
-						<div class="card-title">Yorumlar</div>
-						{#if $session.loggedIn}
+						<div class="card-title">Şərhlər</div>
+						{#if $session.loggedIn && !alreadyCommented}
 							<div class="col-12 col-md-8">
-								<form class="d-flex flex-column">
+								<form class="d-flex flex-column" on:submit|preventDefault={postComment}>
+									<span
+										>{$session.user?.displayName ||
+											$session.user?.email ||
+											$session.user?.phoneNumber}</span
+									>
 									<div class="d-flex">
 										{#each [1, 2, 3, 4, 5] as star, index}
 											<button
@@ -124,22 +189,24 @@
 											</button>
 										{/each}
 									</div>
-
-									<!-- <input
-										class="form-control"
-										type="number"
-										name="point"
-										id="point"
-										min="1"
-										max="5"
-									/> -->
-									<label for="comment">Yorumunuz</label>
+									<label for="comment">Şərhiniz</label>
 									<textarea class="form-control" name="comment" id="comment" />
-									<button class="btn btn-outline-primary mt-3" type="submit">Göndər</button>
+									<button
+										class="btn btn-outline-primary mt-3"
+										type="submit"
+										disabled={btnCommentDisabled || selectedStarPoint == 0}>{btnCommentText}</button
+									>
 								</form>
 							</div>
 						{/if}
-						<div class="card-body">Yorumlar</div>
+						<div class="card-body">
+							{#each comments as comment}
+								<div class="card p-3">
+									<p>{comment.userId} - {comment.star}*</p>
+									<p>{comment.comment}</p>
+								</div>
+							{/each}
+						</div>
 					</div>
 				</div>
 			</div>
@@ -150,6 +217,7 @@
 <style>
 	.card {
 		cursor: auto;
+		min-height: auto;
 	}
 	.star {
 		border: 0px;
@@ -163,8 +231,14 @@
 		margin-block: auto;
 		background: white;
 		padding-inline: 2rem;
+		padding-block: 1rem;
 	}
 	.btnRandevu:hover {
 		background-color: var(--primaryColor);
+	}
+	@media screen and (max-width: 992px) {
+		.btnRandevu {
+			padding-block: 0.5rem;
+		}
 	}
 </style>

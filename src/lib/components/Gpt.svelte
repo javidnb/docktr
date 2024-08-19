4901fc36464fe75e7ebf7f96a4a7569b9804d508
Javid N <cavidnebi@gmail.com>
2024-08-19 23:34:53 +0400

    v$(node -p -e require('../package.json').version)

--- src/lib/components/Gpt.svelte
+++ src/lib/components/Gpt.svelte
@@ -106,7 +106,7 @@
 			if (event.type === 'focus') {
 				setTimeout(() => {
 					const viewportHeight = window.visualViewport?.height || window.innerHeight;
-					if (chatBoxContainer) chatBoxContainer.style.height = `${viewportHeight - 255}px`;
+					if (chatBoxContainer) chatBoxContainer.style.height = `${viewportHeight - 350}px`;
 				}, 300);
 			} else if (event.type === 'blur') {
 				chatBoxContainer.style.height = 'calc(100dvh - 195px)';

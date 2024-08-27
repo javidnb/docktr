9aaa1cc4b8079780e919771df20b4ed61a8a716d
Javid N <cavidnebi@gmail.com>
2024-08-27 14:50:22 +0400

    v$(node -p -e require('../package.json').version)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -40,10 +40,31 @@
 						<div class="w-100 mobileOnly">
 							<button
 								class="btn btn-outline-primary bg-white w-100 py-4 mt-3"
+								style="position: relative; border-radius: 20px; 
+								padding-left: 2rem; background-color: #fffffff0 !important"
 								on:click={() => {
 									goto('/search');
-								}}>Görüş Al</button
+								}}
 							>
+								<div class="d-flex flex-column">
+									<div class="d-flex gap-2 align-items-center" style="font-size: 20px;">
+										<span class="material-symbols-outlined"> library_add </span>
+										Görüş Al
+									</div>
+									<span style="font-size: small; color: gray; text-align: left"
+										>Həkimlərdən yeni görüş</span
+									>
+									<img
+										src="https://ik.imagekit.io/d2nwsj0ktvh/pngwing.com_tMbKE8rfj.png"
+										alt="Get Appointment"
+										style="width: 85px;
+											position: absolute;
+											right: 2rem;
+											top: 50%;
+											transform: translateY(-50%);"
+									/>
+								</div>
+							</button>
 						</div>
 					</div>
 				</div>

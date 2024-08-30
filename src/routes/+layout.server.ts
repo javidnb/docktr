6be29812dc43343a936248e090d1e56577f69381
Javid N <cavidnebi@gmail.com>
2024-08-30 15:52:05 +0400

    v$(node -p -e require('../package.json').version)

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,13 +1,18 @@
 import { session } from '$lib/session';
+import { dataLoading } from '$lib/store/dataStore';
 import type { Cookies } from '@sveltejs/kit';
 
 export async function load({ cookies }: { cookies: Cookies }) {
 	const user = cookies.get('user');
 
 	if (user) {
-		session.set({ user: JSON.parse(user), loggedIn: true });
+		let usr = JSON.parse(user);
+		if (usr.uid) {
+			session.set({ user: JSON.parse(user), loggedIn: true });
+		}
 	} else {
 		session.set({ user: null, loggedIn: false });
+		dataLoading.set(false);
 	}
 
 	return {
